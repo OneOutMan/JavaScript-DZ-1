fa5ca981b3dbe03f4a920cf028e598587bc0b8d2
Daniil <dotnik11@yandex.ru>
2020-04-23 12:14:32 +0300

    фиксация изминений

---
+++ main.js
@@ -0,0 +1,12 @@
+let algebra = a;
+let geography = g;
+let physics = p;
+
+function averageMark(a,g,p) {
+    let a = 5;
+    let g = 3;
+    let p = 3;
+    console.log ( (a + g + p) / 3);
+    let assess = 3.6;
+    averageMark()
+}
